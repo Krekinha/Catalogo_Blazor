b9b0e2d3d8d5d3e1bc0f172218d42ff7cb16585d
Dionison <krekmg@gmail.com>
2020-10-02 16:27:43 -0300

    71. Criando a Web API - CategoriaController

---
+++ Catalogo_Blazor/Server/Controllers/CategoriasController.cs
@@ -0,0 +1,111 @@
+﻿using Catalogo_Blazor.Server.Context;
+using Catalogo_Blazor.Shared.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalogo_Blazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Categorias
+        // Retorna uma lista de Categorias
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Categoria>>> GetCategorias()
+        {
+            return await _context.Categorias.ToListAsync();
+        }
+
+        // GET: api/Categorias/5
+        // Busca e retorna uma Categoria com base em um "id"
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Categoria>> GetCategoria(int id)
+        {
+            var categoria = await _context.Categorias.FindAsync(id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return categoria;
+        }
+
+        // PUT: api/Categorias/5
+        // Atualiza uma Categoria existente
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCategoria(int id, Categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(categoria).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                return Ok(categoria);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CategoriaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Categorias
+        // Cria uma nova Categoria
+        [HttpPost]
+        public async Task<ActionResult<Categoria>> PostCategoria(Categoria categoria)
+        {
+            _context.Categorias.Add(categoria);
+            await _context.SaveChangesAsync();
+
+            //retorna a categoria criada
+            return CreatedAtAction("GetCategoria", new { id = categoria.CategoriaId }, categoria);
+        }
+
+        // DELETE: api/Categorias/5
+        // Busca e exclui uma Categoria com base em um "id"
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Categoria>> DeleteCategoria(int id)
+        {
+            var categoria = await _context.Categorias.FindAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            _context.Categorias.Remove(categoria);
+            await _context.SaveChangesAsync();
+
+            return categoria;
+        }
+
+        private bool CategoriaExists(int id)
+        {
+            return _context.Categorias.Any(e => e.CategoriaId == id);
+        }
+    }
+}
