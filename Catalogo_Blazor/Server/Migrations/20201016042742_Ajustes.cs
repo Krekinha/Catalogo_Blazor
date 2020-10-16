0af305db09c76934b078104f91453651d5bd092c
Dionison <krekmg@gmail.com>
2020-10-16 02:14:51 -0300

    85. Criando o Componente para Incluir Produto

---
+++ Catalogo_Blazor/Server/Migrations/20201016042742_Ajustes.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Catalogo_Blazor.Server.Migrations
+{
+    public partial class Ajustes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Produtos",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Preco",
+                table: "Produtos",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Preco",
+                table: "Produtos");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Produtos",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descricao",
+                table: "Produtos",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200);
+        }
+    }
+}
--- Catalogo_Blazor/Shared/Models/Produto.cs
+++ Catalogo_Blazor/Shared/Models/Produto.cs
@@ -4,13 +4,19 @@ namespace Catalogo_Blazor.Shared.Models
 {
     public class Produto
     {
+        [Key]
         public int ProdutoId { get; set; }
 
+        [Required(ErrorMessage = "Informe o nome do produto")]
         [MaxLength(100)]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Informe a descrição do produto")]
         [MaxLength(200)]
         public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "Informe o preço do produto")]
+        public decimal Preco { get; set; }
 
         public string ImagemUrl { get; set; }
 
