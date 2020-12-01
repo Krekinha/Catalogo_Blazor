27a1a6a6e76b683d1cf6b55cc59b5316a9818ff8
Dionison <krekmg@gmail.com>
2020-11-30 21:21:23 -0300

    95. Implementando um AuthenticationStateProvider Demo

---
+++ Catalogo_Blazor/Client/Auth/DemoAuthStateProvider.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Catalogo_Blazor.Client.Auth
+{
+    public class DemoAuthStateProvider : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            //await Task.Delay(4000);
+            //indicamos se o usuário e seus claims estão autenticados
+            var user = new ClaimsIdentity();
+
+            return await Task.FromResult(new AuthenticationState(
+                new ClaimsPrincipal(user)
+                ));
+        }
+    }
+}
--- Catalogo_Blazor/Client/Program.cs
+++ Catalogo_Blazor/Client/Program.cs
@@ -1,12 +1,10 @@
+using Catalogo_Blazor.Client.Auth;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace Catalogo_Blazor.Client
 {
@@ -18,6 +16,9 @@ namespace Catalogo_Blazor.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, DemoAuthStateProvider>();
 
             await builder.Build().RunAsync();
         }
